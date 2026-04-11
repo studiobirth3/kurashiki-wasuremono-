d5700d7a0f9d5e56d6d7288d696cae30f8af9650
studiobirth3 <studiobirth3@gmail.com>
2026-04-11 18:08:07 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 window.APP_CONFIG = {
-  // ここに Apps Script のウェブアプリURL（.../exec）を貼ってください
-  GAS_ENDPOINT: 'https://script.google.com/macros/s/AKfycbz_EYXi1s0kyrsG0BMIIkj1HRec7ktA853QHqi-H7-I/dev'
+  GAS_ENDPOINT: 'https://script.google.com/macros/s/AKfycbz_EYXi1s0kyrsG0BMIIkj1HRec7ktA853QHqi-H7-I/exec'
 };
