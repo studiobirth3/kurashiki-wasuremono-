0f40f33420e8950c9d871c4d7619e61810e32c73
studiobirth3 <studiobirth3@gmail.com>
2026-04-12 17:00:35 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = {
-  GAS_ENDPOINT: 'https://script.google.com/macros/s/AKfycbx3T0XYaKY7EcSVz6vwNZXoWtx-2wB664PcyDAWu-1tfLd--GQjpOC_advfqpxxFwx9rA/exec'
+  GAS_ENDPOINT: 'https://script.google.com/macros/s/AKfycbyPXjCPFCeM9wlU6fMTHkctpv2mMbOT5uFJDBFYB3MUxaNinYVTdHM5Lw1ctuiPNBRE5Q/exec'
 };
